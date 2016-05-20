357a66ca3e344cf45522f3b54d289f7d669a26c9
Yu-Han Lin <guesslin1986@gmail.com>
2016-05-20 16:46:42 +0800

    Update: 直接用 datetime 做時間運算

--- countdown.py
+++ countdown.py
@@ -17,31 +17,16 @@ def config(cfg='config.cfg'):
 
 def main():
     r = config()
-    lastdays = 0
     target = datetime(r['year'], r['month'], r['day'])
-    tyear = int(target.strftime("%Y"))
-    tdays = int(target.strftime("%j"))
 
     current = datetime.now()
-    cyear = int(current.strftime("%Y"))
-    cdays = int(current.strftime("%j"))
-    if cyear > tyear:
-        print '已經退伍了喔！'
-    elif cyear == tyear:
-        lastdays = tdays - cdays
-    else:
-        for y in xrange(cyear, tyear+1):
-            if y == tyear:
-                lastdays += tdays
-            elif y == cyear:
-                lastdays += (int(datetime(y, 12, 31).strftime("%j")) - cdays)
-            else:
-                lastdays += int(datetime(y, 12, 31).strftime("%j"))
 
-    if lastdays <= 0:
+    last = target - current
+
+    if last.days <= 0:
         print '已經退伍了喔！'
     else:
-        print '退伍剩下{:03d}天'.format(lastdays)
+        print '退伍剩下{:03d}天'.format(last.days+1)
 
 
 if __name__ == '__main__':
